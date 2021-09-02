032b3a58bf1573ce1bf414d58cf750d045f41ab6
rray524 <rray524@gmail.com>
2021-09-03 02:01:58 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+console.log(parseInt('count' + 1));
+
+
+console.log(Math.floor(Math.random() * 90000) + 10000)
+const product = 5;
+const price = '7';
+const subTotal = product * price;
+const tax = subTotal / 10;
+const total = subTotal + tax;
+console.log(parseInt(total));
